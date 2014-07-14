1942033b513c144d228d23e25c54a9524b4ddd2c
李梦琦 <limengqi150@hotmail.com>
2014-07-14 23:27:22 +0800

    Create LMQExtension.h

---
+++ LMQExtension.h
@@ -0,0 +1,12 @@
+//
+//  LMQExtension.h
+//  MyExtension
+//
+//  Created by CNmoosky on 14-7-14.
+//  Copyright (c) 2014年 CNmoosky. All rights reserved.
+//
+
+
+#import "NSObject+Element.h"
+#import "NSObject+DictAndModel.h"
+
